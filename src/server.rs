f63258f8d275e604975caa1943359937f2febb4c
Thomas Forgione <thomas@forgione.fr>
2025-03-03 15:57:36 +0100

    Working on UI

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::result::Result as StdResult;
 
-use serde_json::json;
+use serde_json::{json, Value};
 
 use ergol::prelude::*;
 
@@ -15,8 +15,6 @@ use rocket::fs::NamedFile;
 use rocket::response::content::RawHtml;
 use rocket::{self, Ignite, Rocket, State as S};
 
-use rocket_dyn_templates::{context, Template};
-
 use crate::config::{Config, BLACKLISTED_DATASET};
 use crate::db::Media;
 use crate::db::Species;
@@ -24,9 +22,67 @@ use crate::logger::LogFairing;
 use crate::utils::{pretty_finder, pretty_name};
 use crate::{Db, Result};
 
-/// Index route of our application.
+/// Number of items per page.
+const LIMIT: i64 = 16;
+
+/// Easily return `RawHtml<String>`.
+type Html = RawHtml<String>;
+
+/// Helper trait to be able to easily render json.
+trait Renderable {
+    /// Uses tera to render with json data as context.
+    fn render_json(&self, template_name: &str, value: Value) -> Result<Html>;
+}
+
+impl Renderable for Tera {
+    fn render_json(&self, template_name: &str, value: Value) -> Result<Html> {
+        let context = Context::from_serialize(value)?;
+        Ok(RawHtml(self.render(template_name, &context)?))
+    }
+}
+
+/// Returns the number of species.
+pub async fn count_species(db: &Db) -> Result<i64> {
+    // Count species to know page number
+    let sql = r#"
+        SELECT
+            COUNT(DISTINCT speciess.id)
+        FROM
+            speciess, occurrences, medias
+        WHERE
+            speciess.id = occurrences.species AND
+            occurrences.id = medias.occurrence AND
+            occurrences.dataset_key != $1 AND
+            200 <= medias.status_code AND medias.status_code < 400 AND
+            medias.path IS NOT NULL
+        ;
+    "#;
+
+    let species_count = db
+        .client()
+        .query(sql, &[&BLACKLISTED_DATASET])
+        .await?
+        .into_iter()
+        .next()
+        .unwrap()
+        .get::<usize, i64>(0);
+
+    Ok(species_count)
+}
+
+/// Index route.
 #[get("/")]
-pub async fn index(tera: &S<Tera>, db: Db) -> Result<RawHtml<String>> {
+pub fn index(tera: &S<Tera>) -> Result<Html> {
+    tera.render_json("index.html", json!({}))
+}
+
+/// List the species.
+#[get("/species/list/<page>")]
+pub async fn species(tera: &S<Tera>, db: Db, page: Option<u32>) -> Result<Html> {
+    let page = page.unwrap_or(1);
+
+    let species_count = count_species(&db).await?;
+
     // Beautiful sql request
     let sql = r#"
     SELECT
@@ -40,8 +96,13 @@ pub async fn index(tera: &S<Tera>, db: Db) -> Result<RawHtml<String>> {
         SELECT
             DISTINCT ON (speciess.id)
             speciess.id AS species_id,
-            speciess.species_key AS species_key,
+            speciess.reign AS reign,
+            speciess.phylum AS phylum,
+            speciess.class AS class,
+            speciess.order AS order,
+            speciess.genus AS genus,
             speciess.valid_name AS valid_name,
+            speciess.species_key AS species_key,
             medias.path AS media_path
         FROM
             speciess, occurrences, medias
@@ -59,15 +120,34 @@ pub async fn index(tera: &S<Tera>, db: Db) -> Result<RawHtml<String>> {
         200 <= medias.status_code AND medias.status_code < 400 AND
         medias.path IS NOT NULL
     GROUP BY
-        subquery.species_id, subquery.valid_name, subquery.species_key, subquery.media_path
+        subquery.species_id,
+        subquery.reign,
+        subquery.phylum,
+        subquery.class,
+        subquery.order,
+        subquery.genus,
+        subquery.valid_name,
+        subquery.species_key,
+        subquery.media_path
+    ORDER BY
+        subquery.reign,
+        subquery.phylum,
+        subquery.class,
+        subquery.order,
+        subquery.genus,
+        subquery.valid_name
+    OFFSET
+        $2
+    LIMIT
+        $3
     ;
     "#;
 
+    let offset = (page - 1) as i64 * LIMIT;
     let species = db
         .client()
-        .query(sql, &[&BLACKLISTED_DATASET])
-        .await
-        .unwrap()
+        .query(sql, &[&BLACKLISTED_DATASET, &offset, &LIMIT])
+        .await?
         .into_iter()
         .map(|x| {
             let species_key = x.get::<usize, i64>(1);
@@ -88,32 +168,80 @@ pub async fn index(tera: &S<Tera>, db: Db) -> Result<RawHtml<String>> {
         })
         .collect::<Vec<_>>();
 
-    Ok(RawHtml(tera.render(
-        "index.html",
-        &Context::from_serialize(json!({ "species": species }))?,
-    )?))
+    let max_page = species_count / LIMIT + 1;
+
+    tera.render_json(
+        "species.html",
+        json!({
+            "current_page": page,
+            "species": species,
+            "species_count": species_count,
+            "max_page": max_page,
+            "offset": offset,
+            "limit": LIMIT,
+        }),
+    )
+}
+
+/// Route for visualizing medias for a certain species.
+#[get("/species/key/<species_key>")]
+pub async fn species_by_key(species_key: i64, tera: &S<Tera>, db: Db) -> Result<Html> {
+    let species = Species::get_by_species_key(species_key, &db)
+        .await?
+        .unwrap();
+
+    let sql = r#"
+        SELECT
+            medias.*
+        FROM
+            speciess,
+            occurrences,
+            medias
+        WHERE
+            speciess.species_key = $1 AND
+            occurrences.species = speciess.id AND
+            medias.occurrence = occurrences.id AND
+            200 <= medias.status_code AND medias.status_code < 400
+        ;
+    "#;
+
+    let medias = db.client().query(sql, &[&species_key]).await?;
+    let medias = medias.iter().map(Media::from_row).collect::<Vec<_>>();
+
+    tera.render_json(
+        "species-key.html",
+        json!({
+            "species": species.to_json(&db).await?,
+            "medias": medias,
+        }),
+    )
 }
 
 /// Route for visualising a media.
-#[get("/<species_key>/<occurrence_key>/<media_index>")]
-pub async fn media(species_key: i64, occurrence_key: i64, media_index: i32, db: Db) -> Template {
+#[get("/media/<species_key>/<occurrence_key>/<media_index>")]
+pub async fn media(
+    species_key: i64,
+    occurrence_key: i64,
+    media_index: i32,
+    tera: &S<Tera>,
+    db: Db,
+) -> Result<Html> {
     let species = Species::get_by_species_key(species_key, &db)
-        .await
-        .unwrap()
+        .await?
         .unwrap();
 
-    let media = Media::get_by_id(media_index, &db).await.unwrap().unwrap();
+    let media = Media::get_by_id(media_index, &db).await?.unwrap();
 
-    Template::render(
-        "media",
-        context! {
-            species_pretty_name: pretty_name(&species.valid_name),
-            species_pretty_finder: pretty_finder(&species.valid_name),
-            species_key: species_key,
-            occurrence_key: occurrence_key,
-            media_id: format!("{:04}", media.id),
-            media: media,
-        },
+    tera.render_json(
+        "media.html",
+        json!({
+            "species_pretty_name": pretty_name(&species.valid_name),
+            "species_pretty_finder": pretty_finder(&species.valid_name),
+            "species_key": species_key,
+            "occurrence_key": occurrence_key,
+            "media_id": format!("{:04}", media.id),
+            "media": media,
+        }),
     )
 }
 
@@ -134,7 +262,6 @@ async fn data_files(config: &S<Config>, file: PathBuf) -> Option<NamedFile> {
 /// Starts the web server.
 pub async fn serve() -> StdResult<Rocket<Ignite>, rocket::Error> {
     rocket::build()
-        .attach(Template::fairing())
         .attach(AdHoc::on_ignite("Config", |rocket| async move {
             let config = Config::from_rocket(&rocket);
             rocket.manage(config)
@@ -156,7 +283,17 @@ pub async fn serve() -> StdResult<Rocket<Ignite>, rocket::Error> {
             rocket.manage(tera)
         }))
         .attach(LogFairing)
-        .mount("/", routes![index, media, static_files, data_files,])
+        .mount(
+            "/",
+            routes![
+                index,
+                species,
+                species_by_key,
+                media,
+                static_files,
+                data_files,
+            ],
+        )
         .ignite()
         .await?
         .launch()
